2bf74d4c5d594a60edfab6cf59c603eecdd4f0f2
huami <huami@huamideMacBook-Pro.local>
2016-12-01 17:15:57 +0800

    添加BlockNumber

---
+++ BlockPlayground/BlockPlayground/BlockNumber.h
@@ -0,0 +1,13 @@
+//
+//  BlockNumber.h
+//  BlockPlayground
+//
+//  Created by huami on 16/12/1.
+//  Copyright © 2016年 Linebreak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlockNumber : NSObject
+
+@end
